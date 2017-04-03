64ba64ec038ab265e2c4ca2ac1d4083b35a43540
L-F-A <lf.arsenault@gmail.com>
2017-04-03 02:04:18 -0400

    Update nearPD.py

--- nearPD.py
+++ nearPD.py
@@ -41,7 +41,8 @@ def nearestSPD(A,testPD=False,ModIfNotPD=False):
 	if testPD is False:
 		return Ahat
 	else:
-	#testing if Ahat is positive definite. If not, perhaps tweak it
+	#If testPD, will test if Ahat is positive definite by trying to Cholesky decomposed. 
+	#If not, tweak the matrix
 		p=1
 		k=0.
 		while p!=0:
@@ -55,7 +56,7 @@ def nearestSPD(A,testPD=False,ModIfNotPD=False):
 					p=0
 					return Ahat,False
 				else:
-				#Ahat is not pos. def. Tweak by adding tiny multiple of identity matrix
+				#Ahat is not pos. def. adding diagonal terms
 				#might be costly since calculating eigenvalues every time 
 					k+=1.
 					mineig=np.min(np.linalg.eigvalsh(Ahat))
